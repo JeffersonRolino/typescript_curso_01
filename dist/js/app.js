cc74a9baf2d667df5216016f8ef4124dd99d9854
jeffersonrolino <jefferson.rolino@gmail.com>
2024-05-11 06:21:36 -0300

    Capítulo 01 - Aula 03: Preparando o ambiente e configurando o projeto

---
+++ dist/js/app.js
@@ -0,0 +1,6 @@
+/*
+    Regras de Negócio
+    * Não pode ser modificada depois de criada.
+    * Obrigatoriamente tem uma data, quantidade e valor.
+    * Seu volume é calculado multiplicando-se a quantidade negociada no dia pelo valor negociado.
+*/
