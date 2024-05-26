e51cf8fc70647ba5cbe6db5d1976099471bcbf64
jeffersonrolino <jefferson.rolino@gmail.com>
2024-05-26 19:32:01 -0300

    Capítulo 04 - Aula 04: Avançando com nosso modelo

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -2,4 +2,12 @@ import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
+
+    public adiciona(negociacao: Negociacao){
+        this.negociacoes.push(negociacao);
+    }
+
+    public lista(): Array<Negociacao>{
+        return this.negociacoes;
+    }
 }
